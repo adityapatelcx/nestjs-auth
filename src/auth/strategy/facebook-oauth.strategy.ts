9b2160d72294afb1dff69821e00b2b97740434b0
aditya patel <aditya.patel@nonceblox.com>
2022-05-24 18:53:47 +0530

    create: facebook-oauth strategy and index file for strategy dir

---
+++ src/auth/strategy/facebook-oauth.strategy.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-facebook';
+
+@Injectable()
+export class FacebookOAuthStrategy extends PassportStrategy(
+  Strategy,
+  'facebook',
+) {
+  constructor() {
+    super({
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+      profileFields: ['id', 'name', 'emails'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (err: any, user: any, info?: any) => void,
+  ): Promise<any> {
+    const { id, name, emails } = profile;
+
+    const user = {
+      provider: 'facebook',
+      providerId: id,
+      email: emails ? emails[0].value : 'not present',
+      firstName: name.givenName,
+      lastName: name.familyName,
+      accessToken,
+    };
+
+    done(null, user);
+  }
+}
