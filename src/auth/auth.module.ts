dd638f021542ffbf1a874d32d3549d0f97d3601c
aditya patel <aditya.patel@nonceblox.com>
2022-05-25 14:27:45 +0530

    update: auth-module with necessary imports and providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { FacebookOAuthStrategy, GoogleOAuthStrategy } from './strategy';
+import {
+  FacebookOAuthStrategy,
+  GoogleOAuthStrategy,
+  LocalStrategy,
+  JwtStrategy,
+} from './strategy';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService, GoogleOAuthStrategy, FacebookOAuthStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: `${process.env.JWT_SECRET}`,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    AuthService,
+    GoogleOAuthStrategy,
+    FacebookOAuthStrategy,
+    LocalStrategy,
+    JwtStrategy,
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
