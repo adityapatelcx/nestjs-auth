40b5086bb1add474b342f522a75516108de60fa2
aditya patel <aditya.patel@nonceblox.com>
2022-05-30 14:07:15 +0530

    create: index files for auth auth/dto, auth/guard, auth/interface and auth/strategy

--- src/auth/guard/index.ts
+++ src/auth/guard/index.ts
@@ -1,4 +1,3 @@
 export * from './google-oauth.guard';
 export * from './facebook-oauth.guard';
-export * from './local-auth.guard';
 export * from './jwt-auth.guard';
--- src/auth/strategy/index.ts
+++ src/auth/strategy/index.ts
@@ -1,4 +1,3 @@
 export * from './google-oauth.strategy';
 export * from './facebook-oauth.strategy';
-export * from './local.strategy';
 export * from './jwt.strategy';
