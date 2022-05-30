76bfce57744a6d1b2e353e74ba0258d37305f451
aditya patel <aditya.patel@nonceblox.com>
2022-05-30 14:39:28 +0530

    update: app module with mongoose connection and parameters

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      socketTimeoutMS: 30000,
+      keepAlive: true,
+      autoIndex: false,
+      retryWrites: false,
+    }),
     AuthModule,
-    UsersModule,
+    UserModule,
   ],
 })
 export class AppModule {}
