7bb21a7dfe752fcc2e1b2a515032c15dbb212d05
aditya patel <aditya.patel@nonceblox.com>
2022-05-30 12:29:06 +0530

    create: create, edit and update user dto with index.ts file

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MaxLength(30)
+  @IsNotEmpty()
+  @IsOptional()
+  first_name?: string;
+
+  @IsString()
+  @MaxLength(30)
+  @IsOptional()
+  last_name?: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(32)
+  @IsNotEmpty()
+  password: string;
+
+  @IsNumber()
+  @MaxLength(4)
+  @MinLength(4)
+  @IsOptional()
+  master_pin?: number;
+}
