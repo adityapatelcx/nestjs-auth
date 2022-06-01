af9b601fecabeebb5cf094171017fe2d33a4c003
aditya patel <aditya.patel@nonceblox.com>
2022-06-01 16:47:57 +0530

    create: database -> 1. module --> with configurations for mongodb 2. services 3. index file for database dir

---
+++ src/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { DatabaseService } from './database.service';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        socketTimeoutMS: 30000,
+        keepAlive: true,
+        autoIndex: false,
+        retryWrites: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [DatabaseService],
+  exports: [DatabaseService],
+})
+export class DatabaseModule {}
