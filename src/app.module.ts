4106e25bf19690dbafb9c53bd16a10983d877450
aditya patel <aditya.patel@nonceblox.com>
2022-06-01 16:57:51 +0530

    update: auth module with database module import and removed mongooseModule config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      socketTimeoutMS: 30000,
-      keepAlive: true,
-      autoIndex: false,
-      retryWrites: false,
-    }),
     AuthModule,
     UserModule,
+    DatabaseModule,
   ],
 })
 export class AppModule {}
