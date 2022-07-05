27d89281a86a26cefa5a3fc0c69201b3d1415c53
aditya patel <aditya.patel@nonceblox.com>
2022-07-05 13:40:21 +0530

    EP2-92 create: nft controller with /GET route to get all nfts

---
+++ src/nft/nft.controller.ts
@@ -0,0 +1,52 @@
+import { Controller, Get, HttpStatus, Param, Req, Res } from '@nestjs/common';
+
+import { INft } from './nft.interface';
+import { NftService } from './nft.service';
+
+@Controller('nft')
+export class NftController {
+  constructor(private readonly nftService: NftService) {}
+
+  @Get('/')
+  async getNfts(@Req() request, @Res() response): Promise<INft[]> {
+    try {
+      return response.status(HttpStatus.OK).json([
+        {
+          name: 'Wayfarer Classic',
+          description: 'created to test',
+          id: '#123',
+          rarity: '#9a72da',
+          collectionName: 'example',
+          schemaName: 'example',
+          templateId: '#00700966',
+          tradeId: '#00009',
+          attributes: [
+            'background color black',
+            'eye ball white',
+            'eye color pink',
+          ],
+        },
+        {
+          name: 'Swag Classic',
+          description: 'created to test',
+          id: '#1243',
+          rarity: '#9a72ka',
+          collectionName: 'example',
+          schemaName: 'example',
+          templateId: '#00700566',
+          tradeId: '#00007',
+          attributes: [
+            'background color yellow',
+            'eye ball silver',
+            'eye color black',
+          ],
+        },
+      ]);
+    } catch (error) {
+      if (!error.status)
+        return response.status(500).json({ message: error.message });
+
+      return response.status(error.status).json(error.response);
+    }
+  }
+}
